996de8a392bd7d6e60c6149484e965ca87b1283a
Govind <govindsys1008@gmail.com>
2025-12-03 18:55:07 +0530

    added custom auth using cognito

--- djangoproj/djangoapp/api.py
+++ djangoproj/djangoapp/api.py
@@ -1,2 +1,7 @@
 from ninja import NinjaAPI
+from .auth import CustomAuth
 api = NinjaAPI()
+
+@api.get("/protected-check", auth=CustomAuth())
+def protected_check(request):
+    return {"message" : "protection check"}
